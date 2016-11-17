ce3969b94ff770d32af4581c3ec42bb1d36071a8
David Cavins <david.cavins@gmail.com>
2016-11-17 12:17:42 -0600

    Fix grunt file watch process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 			},
 			styles: {
 				files: ['public/less/*.less'],
-				tasks: ['less:cleancss', 'autoprefixer']
+				tasks: ['less:convertcss', 'postcss']
 			}
 		},
 
